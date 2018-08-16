37fec047007b304fa8c17e487379bb9a6d63e326
Sebas Risco <sebas.risco@gmail.com>
2018-08-16 17:18:45 +0200

    Section 4 finished

--- structs/main.go
+++ structs/main.go
@@ -7,21 +7,50 @@ import (
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
 }
 
 func main() {
 	// 1st way to declare a struct:
-	// alex := person{"Alex", "Anderson"}
+	//alex := person{"Alex", "Anderson"}
 
 	// 2nd way (better, specify the variable names):
 	//alex := person{firstName: "Alex", lastName: "Anderson"}
 
 	// 3rd way (declare with default values -> zero values):
-	var alex person
+	//var alex person
+	//alex.firstName = "Alex"
+	//alex.lastName = "Anderson"
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
 
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	jim.print()
+	(&jim).updateName("Jimmy")
+	// The Go compiler performs an implicit &p on the variable,
+	// so this works too (page 158 of the book)
+	//jim.updateName("Jimmy")
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (p *person) updateName(newFirstName string) {
+	(*p).firstName = newFirstName
+	// The Go compiler performs an implicit *p on the variable,
+	// so this works too
+	//p.firstName = newFirstName
 }
